c3b3f80b9199f7c28f6a02e40fe99d3bb234f12d
Xi Chen <xichen827@gmail.com>
2014-12-15 11:19:50 -0800

    stuff cj did

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# test change
+TrelloClone::Application.config.secret_key_base = '79b71265f32afdb0e15e8a4ccfe5c516f4cfa59a0ee796dcd3dad5f3e015555ee3d996e1ce5a476c28a15a1f5df5ad9ab5ab4c326221ed28617c649b1b939709'
